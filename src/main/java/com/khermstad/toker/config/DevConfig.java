bae99b2ed49698df27392d684113f3b12410a8eb
Kris Hermstad <jan_k_hermstad@homedepot.com>
2018-08-17 16:08:37 -0400

    created application.yml

---
+++ src/main/java/com/khermstad/toker/config/DevConfig.java
@@ -0,0 +1,14 @@
+package com.khermstad.toker.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+
+@Configuration
+@Profile({"dev", "default"})
+public class DevConfig {
+
+    @Autowired
+    Environment environment;
+}
--- src/test/java/com/khermstad/toker/controller/HealthCheckTest.java
+++ src/test/java/com/khermstad/toker/controller/HealthCheckTest.java
@@ -5,11 +5,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -32,11 +29,8 @@ public class HealthCheckTest {
 
     @Test
     public void healthCheck() throws Exception {
-
         String checkResponse = mockMvc.perform(get("/")).andReturn().getResponse().getContentAsString();
-
         Assert.assertEquals("Application is Running", checkResponse);
-
     }
 
     public MockMvc getMockMvc() { return mockMvc; }
