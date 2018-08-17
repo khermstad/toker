108b403985425a188f2b8a09d60d92fa2161442d
Kris Hermstad <jan_k_hermstad@homedepot.com>
2018-08-17 16:22:36 -0400

    added basic H2 configuration

--- src/main/java/com/khermstad/toker/config/DevConfig.java
+++ src/main/java/com/khermstad/toker/config/DevConfig.java
@@ -1,6 +1,7 @@
 package com.khermstad.toker.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
@@ -11,4 +12,6 @@ public class DevConfig {
 
     @Autowired
     Environment environment;
+
+
 }
