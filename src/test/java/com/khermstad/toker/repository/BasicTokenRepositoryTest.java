fb7ef67d5c977383174176cf1a1cb5ba5fbeea2c
Kris Hermstad <jan_k_hermstad@homedepot.com>
2018-08-30 16:09:52 -0400

    refactored tests to cleanup data after each test, allowing for the same variables to be used across test contexts

--- src/test/java/com/khermstad/toker/repository/BasicTokenRepositoryTest.java
+++ src/test/java/com/khermstad/toker/repository/BasicTokenRepositoryTest.java
@@ -11,8 +11,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.util.List;
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ActiveProfiles("unittest")
 @EnableJpaRepositories
@@ -25,33 +23,36 @@ public class BasicTokenRepositoryTest {
     @Autowired
     RepositoryUtil repositoryUtil;
 
+    public String applicationId = "TEST_APP_1";
+    public String tokenValue = "ABC123";
+
     @Test
     public void testBasicTokenInsertion(){
 
         BasicToken basicToken = new BasicToken(
-                "A1",
-                "ABC123",
+                applicationId,
+                tokenValue,
                 false,
                 repositoryUtil.getCurrentTimestamp()
         );
 
         basicTokenRepository.save(basicToken);
-        Assert.assertNotNull((basicTokenRepository.findByApplicationId("A1")));
+        Assert.assertNotNull((basicTokenRepository.findByApplicationId(applicationId)));
     }
 
     @Test
     public void testInsertDuplicateApplicationId(){
 
         BasicToken basicToken1 = new BasicToken(
-                "A1",
-                "ABC123",
+                applicationId,
+                tokenValue,
                 false,
                 repositoryUtil.getCurrentTimestamp()
         );
 
         BasicToken basicToken2 = new BasicToken(
-                "A1",
-                "ABC123",
+                applicationId,
+                tokenValue,
                 false,
                 repositoryUtil.getCurrentTimestamp()
         );
@@ -59,16 +60,16 @@ public class BasicTokenRepositoryTest {
         basicTokenRepository.save(basicToken1);
         basicTokenRepository.save(basicToken2);
 
-        BasicToken basicToken = basicTokenRepository.findByApplicationId("A1");
+        BasicToken basicToken = basicTokenRepository.findByApplicationId(applicationId);
 
         Assert.assertTrue(null != basicToken);
     }
 
     @Test
     public void findByApplicationIdAndValueTest(){
-        BasicToken basicToken = new BasicToken("A1", "ABC123", false, repositoryUtil.getCurrentTimestamp());
+        BasicToken basicToken = new BasicToken(applicationId, tokenValue, false, repositoryUtil.getCurrentTimestamp());
         basicTokenRepository.save(basicToken);
-        BasicToken findByResult = basicTokenRepository.findByApplicationIdAndValue("A1", "ABC123");
+        BasicToken findByResult = basicTokenRepository.findByApplicationIdAndValue(applicationId, tokenValue);
         Assert.assertNotNull(findByResult);
     }
 
--- src/test/java/com/khermstad/toker/service/BasicTokenServiceTest.java
+++ src/test/java/com/khermstad/toker/service/BasicTokenServiceTest.java
@@ -1,9 +1,12 @@
 package com.khermstad.toker.service;
 
+import com.khermstad.toker.constant.BasicTokenGeneratorConstant;
 import com.khermstad.toker.entity.BasicToken;
 import com.khermstad.toker.repository.BasicTokenRepository;
 import com.khermstad.toker.to.BasicTokenResponse;
+import com.khermstad.toker.util.BasicTokenGenerator;
 import com.khermstad.toker.util.RepositoryUtil;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,8 +16,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-// TODO: refactor tests to remove string variables for ApplicationId and Value
-// TODO: Try and remove RepositoryUtil from Test dependencies. place inside class.
 @RunWith(SpringJUnit4ClassRunner.class)
 @ActiveProfiles("unittest")
 @EnableJpaRepositories
@@ -30,17 +31,28 @@ public class BasicTokenServiceTest {
     @Autowired
     RepositoryUtil repositoryUtil;
 
+    @Autowired
+    BasicTokenGenerator basicTokenGenerator;
+
+    public String applicationId = "TEST_APP_1";
+    public String tokenValue = "ABC123";
+
+    @After
+    public void cleanUpPreviousTest(){
+            basicTokenRepository.deleteAll();
+    }
+
     @Test
     public void insertNewBasicTokenTest(){
-        BasicToken basicToken = new BasicToken("A1", "ABC123", true, repositoryUtil.getCurrentTimestamp());
+        BasicToken basicToken = new BasicToken(applicationId, tokenValue, true, repositoryUtil.getCurrentTimestamp());
         BasicTokenResponse basicTokenResponse = basicTokenService.insertNewBasicToken(basicToken);
         Assert.assertTrue(basicTokenResponse.isSuccessful());
     }
 
     @Test
     public void insertDuplicateBasicTokenTest(){
-        BasicToken basicToken = new BasicToken("B1", "ABC123", true, repositoryUtil.getCurrentTimestamp());
-        BasicToken basicToken2 = new BasicToken("B1", "ABC1234", true, repositoryUtil.getCurrentTimestamp());
+        BasicToken basicToken = new BasicToken(applicationId, tokenValue, true, repositoryUtil.getCurrentTimestamp());
+        BasicToken basicToken2 = new BasicToken(applicationId, tokenValue, true, repositoryUtil.getCurrentTimestamp());
 
         BasicTokenResponse basicTokenResponse1 = basicTokenService.insertNewBasicToken(basicToken);
         Assert.assertTrue(basicTokenResponse1.isSuccessful());
@@ -51,73 +63,73 @@ public class BasicTokenServiceTest {
 
     @Test
     public void applicationIdDoesNotExistTest(){
-        basicTokenService.applicationIdExists("C1");
-        Assert.assertFalse(basicTokenService.applicationIdExists("C1"));
+        basicTokenService.applicationIdExists(applicationId);
+        Assert.assertFalse(basicTokenService.applicationIdExists(applicationId));
     }
 
     @Test
     public void applicationIdDoesExistTest(){
-        basicTokenRepository.save(new BasicToken("D1", "abc123", false, repositoryUtil.getCurrentTimestamp()));
-        Assert.assertTrue(basicTokenService.applicationIdExists("D1"));
+        basicTokenRepository.save(new BasicToken(applicationId, tokenValue, false, repositoryUtil.getCurrentTimestamp()));
+        Assert.assertTrue(basicTokenService.applicationIdExists(applicationId));
     }
 
     @Test
     public void updateTokenToEnabledTest(){
-        BasicToken initialToken = new BasicToken("E1", "ABC123", false, repositoryUtil.getCurrentTimestamp());
+        BasicToken initialToken = new BasicToken(applicationId, tokenValue, false, repositoryUtil.getCurrentTimestamp());
         basicTokenService.insertNewBasicToken(initialToken);
-        BasicToken savedToken = basicTokenRepository.findByApplicationId("E1");
+        BasicToken savedToken = basicTokenRepository.findByApplicationId(applicationId);
         Assert.assertFalse(savedToken.isEnabled());
 
         savedToken.setEnabled(true);
         basicTokenService.updateTokenIsEnabled(savedToken);
-        BasicToken updatedToken = basicTokenRepository.findByApplicationId("E1");
+        BasicToken updatedToken = basicTokenRepository.findByApplicationId(applicationId);
         Assert.assertTrue(updatedToken.isEnabled());
     }
 
     @Test
     public void updateTokenToDisabledTest(){
-        BasicToken initialToken = new BasicToken("F1", "ABC123", true, repositoryUtil.getCurrentTimestamp());
+        BasicToken initialToken = new BasicToken(applicationId, tokenValue, true, repositoryUtil.getCurrentTimestamp());
         basicTokenService.insertNewBasicToken(initialToken);
-        BasicToken savedToken = basicTokenRepository.findByApplicationId("F1");
+        BasicToken savedToken = basicTokenRepository.findByApplicationId(applicationId);
         Assert.assertTrue(savedToken.isEnabled());
 
         savedToken.setEnabled(false);
         basicTokenService.updateTokenIsEnabled(savedToken);
-        BasicToken updatedToken = basicTokenRepository.findByApplicationId("F1");
+        BasicToken updatedToken = basicTokenRepository.findByApplicationId(applicationId);
         Assert.assertFalse(updatedToken.isEnabled());
     }
 
     @Test
     public void enableTokenForApplicationIdTest() {
-        BasicToken initialToken = new BasicToken("G1", "ABC123", false, repositoryUtil.getCurrentTimestamp());
+        BasicToken initialToken = new BasicToken(applicationId, tokenValue, false, repositoryUtil.getCurrentTimestamp());
         basicTokenService.insertNewBasicToken(initialToken);
-        BasicToken savedToken = basicTokenRepository.findByApplicationId("G1");
+        BasicToken savedToken = basicTokenRepository.findByApplicationId(applicationId);
         Assert.assertFalse(savedToken.isEnabled());
 
-        BasicTokenResponse basicTokenResponse = basicTokenService.enableTokenForApplicationId("G1");
+        BasicTokenResponse basicTokenResponse = basicTokenService.enableTokenForApplicationId(applicationId);
         Assert.assertTrue(basicTokenResponse.isSuccessful());
     }
 
     @Test
     public void enableTokenForApplicationIdNullTokenTest() {
-        BasicTokenResponse basicTokenResponse = basicTokenService.enableTokenForApplicationId("H1");
+        BasicTokenResponse basicTokenResponse = basicTokenService.enableTokenForApplicationId(applicationId);
         Assert.assertFalse(basicTokenResponse.isSuccessful());
     }
 
     @Test
     public void disableTokenForApplicationTest(){
-        BasicToken initialToken = new BasicToken("I1", "ABC123", false, repositoryUtil.getCurrentTimestamp());
+        BasicToken initialToken = new BasicToken(applicationId, tokenValue, false, repositoryUtil.getCurrentTimestamp());
         basicTokenService.insertNewBasicToken(initialToken);
-        BasicToken savedToken = basicTokenRepository.findByApplicationId("I1");
+        BasicToken savedToken = basicTokenRepository.findByApplicationId(applicationId);
         Assert.assertFalse(savedToken.isEnabled());
 
-        BasicTokenResponse basicTokenResponse = basicTokenService.disableTokenForApplicationId("I1");
+        BasicTokenResponse basicTokenResponse = basicTokenService.disableTokenForApplicationId(applicationId);
         Assert.assertTrue(basicTokenResponse.isSuccessful());
     }
 
     @Test
     public void disableTokenForApplicationIdNullTokenTest(){
-        BasicTokenResponse basicTokenResponse = basicTokenService.disableTokenForApplicationId("J1");
+        BasicTokenResponse basicTokenResponse = basicTokenService.disableTokenForApplicationId(applicationId);
         Assert.assertFalse(basicTokenResponse.isSuccessful());
     }
 
